be841a3c54b9561f61ca3fff60cb0d2d4321e2ba
mahamismail <maham.ismail95@gmail.com>
2023-01-25 15:21:37 -0500

    Initial commit

---
+++ src/UserInterface/HealthCounter.gd
@@ -0,0 +1,25 @@
+extends Panel
+
+var health = Global.health
+onready var health_label = $Amount
+
+signal you_Died()
+
+func _ready():
+	health_label.set_text(str(health))
+		# Check if the game is in splitscreen mode by checking the scene root name.
+	if get_tree().get_root().get_child(0).name == "Splitscreen":
+		var _level_node = get_node(@"../../../../Black/SplitContainer/ViewportContainer1/Viewport1/Level")
+		_level_node.get_node("Player1").connect("lose_health", self, "_lose_health")
+		_level_node.get_node("Player2").connect("lose_health", self, "_lose_health")
+	else:
+		var _player_path = get_node(@"../../Level/Player")
+		_player_path.connect("hit", self, "_lose_health")
+
+func _lose_health():
+	health = health-1
+	health_label.set_text(str(health))
+	print(health)
+	
+	if health == 0:
+		emit_signal("you_Died")
